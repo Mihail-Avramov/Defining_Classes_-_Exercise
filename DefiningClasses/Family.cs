495862e2c5f6d3879151d41c01fe8322251df3f6
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-02 17:42:52 +0200

    Add project files.

---
+++ DefiningClasses/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembersList;
+
+        public Family()
+        {
+            this.familyMembersList = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.familyMembersList.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.familyMembersList.MaxBy(m => m.Age);
+        }
+
+    }
+}
---
+++ DefiningClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+
+    {
+        static void Main()
+        {
+            Family newFamily = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person newPerson = new Person(name, age);
+
+                newFamily.AddMember(newPerson);
+            }
+
+            Person person = newFamily.GetOldestMember();
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
