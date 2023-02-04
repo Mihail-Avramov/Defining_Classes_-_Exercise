7139b2a19f2875809db713aad5cc88998772f83f
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-04 16:07:51 +0200

    pokemon

---
+++ PokemonTrainer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputArguments = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = inputArguments[0];
+                string pokemonName = inputArguments[1];
+                string pokemonElement = inputArguments[2];
+                int pokemonHealth = int.Parse(inputArguments[3]);
+
+                if (trainers.Any(t => t.Name == trainerName))
+                {
+                    Trainer currentTrainer = trainers.First(t => t.Name == trainerName);
+                    currentTrainer.AddPokemon(pokemonName, pokemonElement, pokemonHealth);
+                }
+                else
+                {
+                    Trainer newTrainer = new Trainer(trainerName, pokemonName, pokemonElement, pokemonHealth);
+                    trainers.Add(newTrainer);
+                }
+            }
+
+            input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    trainer.CheckPokemonsElement(input);
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(t => t.BadgesCounter))
+            {
+                Console.WriteLine(trainer.PrintTrainer());
+            }
+        }
+    }
+}
---
+++ PokemonTrainer/Trainer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+		private string name;
+		private int badgesCounter;
+		private List<Pokemon> pokemons = new List<Pokemon>();
+
+		public Trainer(string trainerName, string pokemonName, string pokemonElement, int pokemonHealth)
+        {
+			this.name = trainerName;
+            this.badgesCounter = 0;
+            AddPokemon(pokemonName,pokemonElement,pokemonHealth);
+        }
+
+		public List<Pokemon> Pokemons
+		{
+			get { return pokemons; }
+		}
+
+		public int BadgesCounter
+        {
+			get { return badgesCounter; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+		}
+
+        public void AddPokemon(string pokemonName, string pokemonElement, int pokemonHealth)
+        {
+            this.Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+        }
+
+        public void CheckPokemonsElement(string pokemonElement)
+        {
+            if (this.pokemons.Any(p => p.Element == pokemonElement))
+            {
+                this.badgesCounter++;
+            }
+            else
+            {
+                foreach (var pokemon in this.pokemons)
+                {
+                    pokemon.Health -= 10;
+                }
+                this.pokemons.RemoveAll(p => p.Health <= 0);
+            }
+        }
+
+
+        public string PrintTrainer()
+        {
+            return $"{this.Name} {this.BadgesCounter} {this.Pokemons.Count}";
+        }
+    }
+}
